855d3e320408b828c3079922a94951f945ea26b9
Emmanuel Appah <climaxating@gmail.com>
2025-04-19 01:04:03 +0100

    Added scalar for docs

--- src/ssml/api/server.py
+++ src/ssml/api/server.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from .routes import router
+from scalar_fastapi import get_scalar_api_reference
+
 
 app = FastAPI(
     title="SSML Generator",
@@ -9,3 +11,8 @@ app = FastAPI(
     license_info={"name": "MIT License"},
 )
 app.include_router(router, prefix="/api", tags=["ssml"])
+
+
+@app.get("/scalar", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(openapi_url=app.openapi_url, title=app.title)
